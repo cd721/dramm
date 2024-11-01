fc14d8e6f82b3b340220793d646c1744086f51ac
cd721 <cdemario@stevens.edu>
2024-11-01 13:21:54 -0400

    intial commit - skeleton of app w firebase auth

---
+++ src/components/SignOut.jsx
@@ -0,0 +1,11 @@
+import {doSignOut} from '../firebase/FirebaseFunctions';
+
+const SignOut = () => {
+  return (
+    <button className='button' type='button' onClick={doSignOut}>
+      Sign Out
+    </button>
+  );
+};
+
+export default SignOut;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App.jsx";
+import { initializeApp } from "firebase/app";
+import fbconfig from "./firebase/FirebaseConfig.js";
+
+import { BrowserRouter } from "react-router-dom";
+
+const app = initializeApp(fbconfig);
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);

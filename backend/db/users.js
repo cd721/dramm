ad3980c0799c212d4a7702cc9935944474264112
Jo-Anne Rivera <riverajoanne231@gmail.com>
2024-12-16 16:45:02 -0500

    Users xss fix

--- backend/db/users.js
+++ backend/db/users.js
@@ -17,7 +17,7 @@ const exportedMethods = {
         if (!user) throw new Error(`User with ID ${id} not found`);
         return user;
     },
-
+    // NEED FIREBASE TO VALIDATE IF UID EXISTS ON FIREBASE
     async addUserIfNotExists(uid) {
         if (!uid) throw new Error('User ID is required');
         try {

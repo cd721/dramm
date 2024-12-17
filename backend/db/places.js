0a574b0cf4d401c32c990c78aa87b17d3a5fcacb
cd721 <55518903+cd721@users.noreply.github.com>
2024-12-17 18:04:42 -0500

    Update places.js

--- backend/db/places.js
+++ backend/db/places.js
@@ -5,7 +5,7 @@ const client = redis.createClient({
  url: 'redis://cache:6379', 
       socket: {
     connectTimeout: 50000,
-  },
+  }
 });await client.connect().then(() => { });
 
 let exportedMethods = {

c4de354981b7ab5c3c73570bcd68f080a3020e98
Jo-Anne Rivera <riverajoanne231@gmail.com>
2024-12-17 23:27:53 -0500

    Witchcraft

--- backend/db/redisClient.js
+++ backend/db/redisClient.js
@@ -2,14 +2,14 @@ import redis from 'redis';
 
 const client = redis.createClient({
   socket: {
-    host: "cache", // cache since Redis is running in Docker Compose
+    host: process.env.REDIS_HOST || '127.0.0.1', // Defaults to localhost if REDIS_HOST is not set
     port: 6379,
     connectTimeout: 50000,
   },
 });
 
 client.on('error', (err) => console.error('Redis Client Error:', err));
-client.on('connect', () => console.log('Connected to Redis'));
+client.on('connect', () => console.log('Connected to Redis!'));
 
 await client.connect();
 

ce0750e00a52efb40fbde5b1ac3f09e1dd829403
cd721 <cdemario@stevens.edu>
2024-12-18 18:10:05 -0500

    cahnge serverURL

--- backend/config/settings.js
+++ backend/config/settings.js
@@ -1,4 +1,4 @@
 export const mongoConfig = {
-  serverUrl: 'mongodb://localhost:27017/',
+  serverUrl: `mongodb+srv://dramm:${encodeURIComponent(process.env.MONGO_INITDB_ROOT_PASSWORD)}@554-final-project.xcldy.mongodb.net/dramm?retryWrites=true&w=majority&appName=554-Final-Project`,,
   database: 'dramm'
 };
